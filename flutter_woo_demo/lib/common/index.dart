f2437b2a625c1f0bd783adcb5ce6cce59841bc85
Alexander <1023954998@qq.com>
2023-05-09 08:58:02 +0800

    -> 按钮组件

--- flutter_woo_demo/lib/common/index.dart
+++ flutter_woo_demo/lib/common/index.dart
@@ -7,3 +7,4 @@ export './values/index.dart';
 export './style/index.dart';
 export './widgets/index.dart';
 export './i18n/index.dart';
+export './extension/index.dart';
--- flutter_woo_demo/lib/pages/style/style_index/view.dart
+++ flutter_woo_demo/lib/pages/style/style_index/view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_woo_demo/common/i18n/index.dart';
+import 'package:flutter_woo_demo/common/widgets/button.dart';
 import 'package:flutter_woo_demo/common/widgets/icon.dart';
 import 'package:get/get.dart';
 import 'package:flutter_woo_demo/common/index.dart';
@@ -12,6 +13,12 @@ class StyleIndexPage extends GetView<StyleIndexController> {
   Widget _buildView() {
     return Column(
       children: [
+        ButtonWidget.primary(
+          "主要的",
+          width: 70,
+          height: 40,
+          onTap: () {},
+        ),
         ListTile(
             leading: ImageWidget.url(
                 "https://ducafecat.oss-cn-beijing.aliyuncs.com/wp-content/uploads/2022/02/90bb74497f090c48e1df1ec1ca31fb11-450x450.jpg"),
