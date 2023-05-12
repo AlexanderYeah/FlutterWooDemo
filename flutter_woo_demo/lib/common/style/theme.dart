7693da06101b223310922f205e2d36c608c1bcb6
Alexander <1023954998@qq.com>
2023-05-12 16:47:16 +0800

    -> 设置系统栏样式

--- flutter_woo_demo/lib/common/style/theme.dart
+++ flutter_woo_demo/lib/common/style/theme.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './index.dart';
 
 class AppTheme {
   // 亮色
-  static ThemeData light =
-      ThemeData(colorScheme: lightColorScheme, fontFamily: "Montserrat");
+  static ThemeData light = ThemeData(
+      colorScheme: lightColorScheme,
+      fontFamily: "Montserrat",
+      appBarTheme:
+          const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.dark));
   // 暗色主题
-  static ThemeData dark =
-      ThemeData(colorScheme: darkColorScheme, fontFamily: "Montserrat");
+  static ThemeData dark = ThemeData(
+      colorScheme: darkColorScheme,
+      fontFamily: "Montserrat",
+      appBarTheme:
+          const AppBarTheme(systemOverlayStyle: SystemUiOverlayStyle.light));
 }
--- flutter_woo_demo/lib/global.dart
+++ flutter_woo_demo/lib/global.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_woo_demo/common/services/user.dart';
 import 'package:get/get.dart';
@@ -14,6 +15,8 @@ class Global {
     FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
     // 实例化存储类
     await Storage().init();
+    // 设置系统样式UI
+    setSystemUi();
     // 初始化
     Loading();
     // 依赖注入 实例化网络请求工具
@@ -22,6 +25,44 @@ class Global {
     Get.put<UserService>(UserService());
 
     // Get.put 方式直接注入
+  }
+
+// 系统样式
+  static void setSystemUi() {
+    if (GetPlatform.isMobile) {
+      // 屏幕方向 竖直上
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+      // 透明状态栏
+      // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      //   statusBarColor: Colors.transparent, // transparent status bar
+      // ));
+    }
+
+    if (GetPlatform.isAndroid) {
+      // 去除顶部系统下拉和底部系统按键
+      // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+      // 去掉底部系统按键
+      // SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      //     overlays: [SystemUiOverlay.bottom]);
+
+      // 自定义样式
+      SystemUiOverlayStyle systemUiOverlayStyle = const SystemUiOverlayStyle(
+        // 顶部状态栏颜色
+        statusBarColor: Colors.transparent,
+        // 该属性仅用于 iOS 设备顶部状态栏亮度
+        // statusBarBrightness: Brightness.light,
+        // 顶部状态栏图标的亮度
+        // statusBarIconBrightness: Brightness.light,
+
+        // 底部状态栏与主内容分割线颜色
+        systemNavigationBarDividerColor: Colors.transparent,
+        // 底部状态栏颜色
+        systemNavigationBarColor: Colors.white,
+        // 底部状态栏图标样式
+        systemNavigationBarIconBrightness: Brightness.dark,
+      );
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    }
   }
 }
 
